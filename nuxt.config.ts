068a989878d4f791868a1e7434473e6145a85fce
Michel Carlos <mcarlos@littleotterhealth.com>
2023-11-23 14:49:30 -0300

    disable ssr to use spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  ssr: false,
   modules: ["@nuxtjs/tailwindcss", "nuxt-primevue", "@nuxtjs/color-mode"],
   primevue: {
     cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
